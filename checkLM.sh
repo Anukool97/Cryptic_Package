04d60bcae3a63b1c9ad1f3e0cba14dc9691eb372
Anukool Srivastava <38781218+Anukool97@users.noreply.github.com>
2022-05-21 10:05:14 +0530

    Update checkLM.sh

--- checkLM.sh
+++ checkLM.sh
@@ -1,15 +1,15 @@
 #!/bin/ksh
 
 x=$1
-DB_USER="gAAAAABhq0fYyc-_LfjAweFWx7crBBrSq4zIa7AbmS5zpB34aCC7qOk6R7VqA8IHWAaQyljJmi0jTMYLTfSy821u0nJdsPS1Zg=="
+DB_USER="YOUR ENCRYPTED TEXT WILL COME HERE"
 
 USER=`python Decrypt_func.py $x  $DB_USER | tail -1`
 
-DB_PASS="gAAAAABhq0jo7H0gtPdFUnE-nOExbj71tn9nAW9dxRQ8uVKj6odfojndZkhRmNijlOIkgu1soHSJKMnSFjsHYbxN3zKIhfI-kg=="
+DB_PASS="YOUR ENCRYPTED TEXT WILL COME HERE"
 
 PASS=`python Decrypt_func.py $x  $DB_PASS | tail -1`
 
-INSTANCE="gAAAAABhq0l7z62r9_BJW0Adc3GEcgUbaT5q8PgyWNrJmsZznTqN2Tx7D61VTC2xgkzVmtQPjjGRx3QOYh3_B71lYEAXne0n3Q=="
+INSTANCE="YOUR ENCRYPTED TEXT WILL COME HERE"
 
 INSTANCE_=`python Decrypt_func.py $x  $INSTANCE | tail -1`
 
@@ -21,8 +21,6 @@ echo ${INSTANCE_}
 
 
 
-#DB_USER=PRD1OMS
-#DB_PASS=PT21OMS
-#INSTANCE=PETOMS
+
 
 
